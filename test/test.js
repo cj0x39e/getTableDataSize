95c00c9f4ef301009df6a037af260c35e641e2fe
taozhi <taozhi1028@gmail.com>
2018-04-21 13:04:23 +0800

    :white_check_mark: 新增测试用例

--- test/test.js
+++ test/test.js
@@ -21,12 +21,34 @@ describe('getTableDataSize 参数及返回值测试', function() {
         expect(() => getTableDataSize(true)).to.throw(/columns/);
     });
 
+    it('如果 columns 某一个列的配置未定义 field 相关信息，程序正常运行', function () {
+        const columns = [
+            { label: '1', field: 'name' },
+            { label: '2', }
+        ];
+        const data = [
+            { name: '张三' },
+            { name: '李四' }
+        ];
+
+        expect(() => getTableDataSize(columns, data)).to.not.throw();
+    });
+
     it('data 参数不是数组应该抛出错误', function() {
         expect(() => getTableDataSize([], null)).to.throw(/data/);
         expect(() => getTableDataSize([], '')).to.throw(/data/);
         expect(() => getTableDataSize([], {})).to.throw(/data/);
         expect(() => getTableDataSize([], 1)).to.throw(/data/);
         expect(() => getTableDataSize([], true)).to.throw(/data/);
+    });
+
+    it('options 参数不是对象应该能正常运行', function () {
+        expect(() => getTableDataSize(copyColumns(), data, null)).to.not.throw();
+        expect(() => getTableDataSize(copyColumns(), data, '')).to.not.throw();
+        expect(() => getTableDataSize(copyColumns(), data, [])).to.not.throw();
+        expect(() => getTableDataSize(copyColumns(), data, 1)).to.not.throw();
+        expect(() => getTableDataSize(copyColumns(), data, true)).to.not.throw();
+        expect(() => getTableDataSize(copyColumns(), data, () => {})).to.not.throw();
     });
 
     it('返回值应该包含 totalWidth、unfrozenWidth、frozenWidth、calculateColumnAmount、notCalculateColumnAmount，且都为数值', function () {
@@ -40,6 +62,20 @@ describe('getTableDataSize 参数及返回值测试', function() {
     it('返回值 totalWidth 为 unfrozenWidth 和 frozenWidth 之和', function () {
         expect(result.unfrozenWidth + result.frozenWidth).to.be.equal(result.totalWidth);
     });
+
+    it('返回值 calculateColumnAmount 与 notCalculateColumnAmount 之和为 columns.length', function () {
+        expect(result.calculateColumnAmount + result.notCalculateColumnAmount).to.be.equal(columns.length);
+    });
+
+    it('如果 columns 里有某一列用户配置了宽度，则 notCalculateColumnAmount 为 1, calculateColumnAmount 为 columns.length - 1', function () {
+        const columns = copyColumns();
+        columns[0].width = 100;
+
+        const rs = getTableDataSize(columns, data);
+
+        expect(rs.calculateColumnAmount).to.be.equal(columns.length - 1);
+        expect(rs.notCalculateColumnAmount).to.be.equal(1);
+    });
 });
 
 describe('getTableDataSize 配置测试', function() {
@@ -47,10 +83,37 @@ describe('getTableDataSize 配置测试', function() {
     it('配置 columnMaxWidth 后，每列宽度不能超过该值', function () {
         const columns = copyColumns();
         getTableDataSize(columns, data, { columnMaxWidth: 1 });
+
         expect(columns.every(c => c.width <= 1)).to.equal(true);
     });
 
-    it('配置 widthAlias 后，所有列的 widthAlias 字段值相加等于返回的 totalWidth', function () {
+    it('配置 columnRedundancyWidth 后，对比相同的数据未配置时，计算结果每列宽度应增加该宽度', function () {
+        const columns = copyColumns();
+        const columns2 = copyColumns();
+
+
+        getTableDataSize(columns, data, { columnRedundancyWidth: 1 });
+        getTableDataSize(columns2, data);
+
+        const rs = columns.every((c, i) => c.width - columns2[i].width === 1);
+
+        expect(rs).to.equal(true);
+    });
+
+    it('配置一个较大的 font 值，计算结果与默认配置应该有所不同', function () {
+        const columns = copyColumns();
+        const columns2 = copyColumns();
+
+
+        getTableDataSize(columns, data, { font: 'small-caps bold 44px/1 sans-serif' });
+        getTableDataSize(columns2, data);
+
+        const rs = columns.every((c, i) => c.width !== columns2[i].width);
+
+        expect(rs).to.equal(true);
+    });
+
+    it('配置 widthAlias 后，所有列的 widthAlias 字段值有值且之和等于 totalWidth', function () {
         const columns = copyColumns();
         const widthAlias = 'myWidth';
         const result = getTableDataSize(columns, data, { widthAlias: widthAlias });
@@ -58,7 +121,8 @@ describe('getTableDataSize 配置测试', function() {
         expect(sum).to.equal(result.totalWidth);
     });
 
-    it('配置 labelAlias 后，一份 data, 给定两个不同 `labelAlias` 的 columns 并设置其内容长于行的内容，计算的 totalWidth 一定相等', function () {
+    it('配置 labelAlias 后，对于相同的 data, 给定两个不同 `labelAlias` 的 columns 的配置' +
+        '并设置 columns 的 `labelAlias` 的内容大于每行该列的内容，最后两份配置得出的 totalWidth 一定相等', function () {
         const columns = copyColumns();
         const columns2 = copyColumns();
         const labelAlias = 'myLabel';
@@ -71,7 +135,7 @@ describe('getTableDataSize 配置测试', function() {
         expect(result.totalWidth === result2.totalWidth).to.equal(true);
     });
 
-    it('配置 fieldAlias 后，一份 data, 给定两个不同 `fieldAlias` 的 columns，计算的 totalWidth 一定相等', function () {
+    it('配置 fieldAlias 后，对于相同的 data, 给定两个不同 `fieldAlias` 的 columns，计算的 totalWidth 一定相等', function () {
         const columns = copyColumns();
         const columns2 = copyColumns();
         const fieldAlias = 'fieldAlias';
@@ -84,17 +148,18 @@ describe('getTableDataSize 配置测试', function() {
         expect(result.totalWidth === result2.totalWidth).to.equal(true);
     });
 
-    it('配置 frozenAlias 后，一份 data, 指定相同的列固定，只是使用不同的字段，计算的 frozenWidth 一定相等', function () {
-        const columns = copyColumns();
-        const columns2 = copyColumns();
-        const frozenAlias = 'frozenAlias';
-        const frozenAlias2 = 'frozenAlias2';
-        columns.map(c => { c[frozenAlias] = true; return c;});
-        columns2.map(c => { c[frozenAlias2] = true; return c;});
-        const result = getTableDataSize(columns, data, { frozenAlias: frozenAlias });
-        const result2 = getTableDataSize(columns2, data, { frozenAlias: frozenAlias2 });
+    it('配置 frozenAlias 后，对于相同的 data, 指定相同的列固定，给定两个不同 `frozenAlias` 的 columns，计算的 frozenWidth 一定相等',
+        function () {
+            const columns = copyColumns();
+            const columns2 = copyColumns();
+            const frozenAlias = 'frozenAlias';
+            const frozenAlias2 = 'frozenAlias2';
+            columns.map(c => { c[frozenAlias] = true; return c;});
+            columns2.map(c => { c[frozenAlias2] = true; return c;});
+            const result = getTableDataSize(columns, data, { frozenAlias: frozenAlias });
+            const result2 = getTableDataSize(columns2, data, { frozenAlias: frozenAlias2 });
 
-        expect(result.frozenWidth === result2.frozenWidth).to.equal(true);
+            expect(result.frozenWidth === result2.frozenWidth).to.equal(true);
     });
 });
 
