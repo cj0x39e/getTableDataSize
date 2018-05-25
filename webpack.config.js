d1ae569d291cd6de2d5c88afa70bb74a9973e4e3
tasstao <tasstao@boyaa.com>
2018-05-25 16:02:56 +0800

    :bookmark: release 1.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const packageInfo = require('./package.json');
 
 module.exports = {
     entry: './src/index.js',
@@ -7,5 +9,19 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         filename: 'gettabledatasize.js',
         libraryTarget: 'umd'
-    }
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }]
+        }]
+    },
+    plugins: [
+        new webpack.BannerPlugin(`${packageInfo.name}@${packageInfo.version} | ${packageInfo.homepage}`),
+    ]
 };
